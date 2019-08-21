e6d7622e4eb7e4f283a165753a939aabbaf7844e
doxterpepper <dockconnellyoneal@gmail.com>
2019-08-21 17:03:04 -0400

    Moving lines to their own module

--- doc/implementors/core/fmt/trait.Display.js
+++ doc/implementors/core/fmt/trait.Display.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["bauxite"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"bauxite/struct.Box.html\" title=\"struct bauxite::Box\">Box</a>",synthetic:false,types:["bauxite::Box"]},];
 implementors["lib"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"lib/struct.Box.html\" title=\"struct lib::Box\">Box</a>",synthetic:false,types:["lib::Box"]},];
 
             if (window.register_implementors) {
--- doc/source-files.js
+++ doc/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["bauxite"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["bauxite"] = {"name":"","files":["lib.rs","lines.rs"]};
 sourcesIndex["lib"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
