33e6db63f62177406bfad6ebdae9173778c9b63f
doxterpepper <dockconnellyoneal@gmail.com>
2019-08-21 17:24:51 -0400

    Adding an example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! Wrap terminal output with boxes
+//! # Example
+//! ```
+//! let lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//! eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+//! nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+//! dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+//! Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+//! id est laborum.";
+//!
+//! let message = String::from(lorem_ipsum);
+//! let boxed_message = bauxite::Box(message).padding(3)
+//!     .alignment(Alignment::Left);
+//! println!("{}", boxed_message);
+//! ```
 use std::fmt;
 use std::cmp::max;
 
